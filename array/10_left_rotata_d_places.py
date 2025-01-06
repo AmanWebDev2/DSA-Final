fbeb34996edc9f71524c786d41bbf78e5fb649e7
AmaN <webdevaman2@gmail.com>
2025-01-06 23:25:48 +0530

    left rotatoe by d place approach 2

--- array/10_left_rotata_d_places.py
+++ array/10_left_rotata_d_places.py
@@ -16,6 +16,30 @@ def left_rotate_d_places(nums: list[int], d: int):
 
 
 arr = [1, 2, 3, 4, 5, 6]
-d = 7
-left_rotate_d_places(arr, d)
+d = 4
+# left_rotate_d_places(arr, d)
+# print(arr)
+
+"""
+    T.C -> O(d) + O(n-d) + O(n) => O(2n) => O(n)
+    S.C -> O(1)
+"""
+
+
+def left_rotate_d_places2(nums: list[int], d: int):
+    n = len(nums)
+    d = d % n
+    sort(nums, 0, d - 1)
+    sort(nums, d, n - 1)
+    sort(nums, 0, n - 1)
+
+
+def sort(nums: list[int], i: int, j: int):
+    while i < j:
+        nums[i], nums[j] = nums[j], nums[i]
+        i += 1
+        j -= 1
+
+
+left_rotate_d_places2(arr, d)
 print(arr)
